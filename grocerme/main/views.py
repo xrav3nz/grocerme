79b88d89f904841f830126453a9a90bcd2dbe7cb
Marc Lo <lo.hungyui@gmail.com>
2016-01-23 06:32:32 -0500

    Created landing pages for fridge, receipes, grocery cart, and nutritions

--- grocerme/main/views.py
+++ grocerme/main/views.py
@@ -4,3 +4,19 @@ from . import main_blueprint
 @main_blueprint.route('/')
 def index():
     return render_template('index.html')
+
+@main_blueprint.route('/my_fridge')
+def my_fridge():
+    return render_template('my_fridge.html')
+
+@main_blueprint.route('/my_receipes')
+def my_receipes():
+    return render_template('my_receipes.html')
+
+@main_blueprint.route('/my_grocery_cart')
+def my_grocery_cart():
+    return render_template('my_grocery_cart.html')
+
+@main_blueprint.route('/my_nutrition')
+def my_nutrition():
+    return render_template('my_nutrition.html')
