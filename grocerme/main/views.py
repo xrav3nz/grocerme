3fc0d2772818376cb90418da161269a375c72855
Kyle Zhao <kyle_0420@hotmail.com>
2016-01-23 08:40:01 -0500

    rename routes and dynamic urls in templates

--- grocerme/main/views.py
+++ grocerme/main/views.py
@@ -5,18 +5,18 @@ from . import main_blueprint
 def index():
     return render_template('index.html')
 
-@main_blueprint.route('/my_fridge')
+@main_blueprint.route('/fridge')
 def my_fridge():
     return render_template('my_fridge.html')
 
-@main_blueprint.route('/my_receipes')
+@main_blueprint.route('/receipes')
 def my_receipes():
     return render_template('my_receipes.html')
 
-@main_blueprint.route('/my_grocery_cart')
+@main_blueprint.route('/grocery_cart')
 def my_grocery_cart():
     return render_template('my_grocery_cart.html')
 
-@main_blueprint.route('/my_nutrition')
+@main_blueprint.route('/nutrition')
 def my_nutrition():
     return render_template('my_nutrition.html')
