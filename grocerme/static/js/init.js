543d06430c6c400525ffa3cd9a942313b6ee5622
Marc Lo <lo.hungyui@gmail.com>
2016-01-23 19:49:33 -0500

    Fixed login screen

--- grocerme/static/js/init.js
+++ grocerme/static/js/init.js
@@ -93,7 +93,13 @@ ko.applyBindings(new FridgeViewModel());
 
 
 $(document).ready(function() {
-	    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+	
+	$('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15 // Creates a dropdown of 15 years to control year
+	});
+
+	// the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
 
 	$('.button-collapse').sideNav();
